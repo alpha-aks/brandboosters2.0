a3bf4431a45ed6bd01447476841a056d7dcf0ba5
alpha-aks <149251160+alpha-aks@users.noreply.github.com>
2025-11-22 21:08:37 +0530

    Update favicon and add social media meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,33 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <title>OCHI - Presentation Design Agency</title>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <meta name="theme-color" content="#000000" />
+        
+        {/* Primary Meta Tags */}
+        <meta name="title" content="BrandBoosters - Creative Branding Agency" />
+        <meta name="description" content="We help brands grow through strategic design and development solutions." />
+        
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.brandboosters.marketing/" />
+        <meta property="og:title" content="BrandBoosters - Creative Branding Agency" />
+        <meta property="og:description" content="We help brands grow through strategic design and development solutions." />
+        <meta property="og:image" content="https://www.brandboosters.marketing/logo.png" />
+        
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.brandboosters.marketing/" />
+        <meta property="twitter:title" content="BrandBoosters - Creative Branding Agency" />
+        <meta property="twitter:description" content="We help brands grow through strategic design and development solutions." />
+        <meta property="twitter:image" content="https://www.brandboosters.marketing/logo.png" />
+        
+        {/* WhatsApp */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
       <body>
         <Main />
         <NextScript />
